73d3c94e384c510a7585ab998af356a61d9324e5
lauri lehtola <lauri.lehtola@relexsolutions.com>
2024-06-12 14:11:54 +0300

    More tests

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,11 @@
 import numpy as np
 from collections.abc import Iterable
 
-def create_point(x, y, z):
-    return np.array([x, y ,z, 1])
+def create_point(xyz):
+    return np.append(np.array(xyz),1)
 
-def create_vector(x, y, z):
-    return np.array([x, y, z, 0])
+def create_vector(xyz):
+    return np.append(np.array(xyz),0)
 
 def _is_point(vector):
     return vector[3] == 1
@@ -21,3 +21,9 @@ def _is_close(a, b, tol=1e-5):
             return np.abs(a - b).all() < tol
     else:
         return np.abs(a - b) < tol
+    
+def add_tuples(a, b):
+    return a + b
+
+def substract_tuples(a, b):
+    return a - b
--- tests/test_points_vectors.py
+++ tests/test_points_vectors.py
@@ -1,6 +1,8 @@
 import pytest
 import numpy as np
-from src.utils import _is_point, create_point, _is_vector, create_vector, _is_close
+import sys
+sys.path.append('/Users/Lauri.Lehtola/private_projects/ray_tracing')
+from src.utils import _is_point, create_point, _is_vector, create_vector, _is_close, add_tuples, substract_tuples
 
 @pytest.mark.parametrize(
     "coordinates, expected",
@@ -13,7 +15,7 @@ def test_is_point(coordinates, expected):
     assert _is_point(coordinates) == expected
 
 def test_create_point():
-    assert np.array_equal(create_point(1, 2, 3), np.array([1, 2, 3, 1]))
+    assert np.array_equal(create_point((1, 2, 3)), np.array([1, 2, 3, 1]))
                           
 @pytest.mark.parametrize(
     "coordinates, expected",
@@ -27,8 +29,23 @@ def test_is_vector(coordinates, expected):
 
 def test_create_vector():
     # assert np.array_equal(create_vector(1, 2, 3), np.array([1, 2, 3, 0]))
-    assert _is_close(create_vector(1, 2, 3), np.array([1, 2, 3, 0]))
+    assert _is_close(create_vector((1, 2, 3)), np.array([1, 2, 3, 0]))
 
 def test_is_close():
     assert _is_close(1.0, 1.000001)
     assert not _is_close(1.0, 1.00001)
+
+def test_add_tuples():
+    assert _is_close(add_tuples(np.array([1,2,3,1]), np.array([1,1,1,0])), np.array([2,3,4,1]))
+
+def test_substract_tuples():
+    assert _is_close(substract_tuples(np.array([1,2,3,1]), np.array([1,1,1,0])), np.array([0,1,2,1])) 
+
+# @pytest.mark.parametrize(
+#     "tuples, expected",
+#     [
+#         (np.array([1, 2, 3, 0]), True),
+#         (np.array([1, 2, 3, 1]), False),
+#     ],
+# )
+# def test_add_point_and_vector():  
